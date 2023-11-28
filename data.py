d242748142b2c677e91ccfdac21539f439e7586e
fairyyyyyyyy <126807350+fairyyyyyyyy@users.noreply.github.com>
2023-11-28 16:46:33 -0500

    zz

--- data.py
+++ data.py
@@ -1,5 +1,6 @@
 import re
 import csv
+from datetime import datetime
 
 # Read existing transaction IDs from the previous CSV file
 existing_transaction_ids = set()
@@ -38,12 +39,15 @@ def extract_item_level(item_name):
     if item_level_match:
         return item_level_match.group(0)
 
-# Function to extract the item name
+# Function to extract the item name and remove commas
 def extract_item_name(item_name):
     # Extract the item name before '&rr' or '&cp'
     item_name_match = re.search(r'^.*?(?=&(?:cp|rr))', item_name)
     if item_name_match:
-        return item_name_match.group(0)
+        extracted_name = item_name_match.group(0)
+        # Remove commas from the extracted item name
+        extracted_name = extracted_name.replace(',', '')
+        return extracted_name
 
 # Function to extract the quality of the item
 def extract_item_quality(item_name):
@@ -115,44 +119,55 @@ def extract_item_type2(item_name):
 # Find all matches using the pattern in the trimmed Lua data
 matches = re.findall(pattern, trimmed_lua_data)
 
+# List to store the rows before sorting
+rows_to_write = []
+
+for match, quantity_match in zip(matches, quantity_matches):
+    # Extract item quantity from the quantity match
+    item_quantity_1, item_quantity_2 = quantity_match
+
+    # Convert Unix timestamp to formatted date and time
+    transaction_date_time = datetime.utcfromtimestamp(int(match[3])).strftime('%Y-%m-%d %H:%M:%S')
+
+    # Append 'g' after the total price
+    total_price = f"{match[4]}g"
+
+    # Extract item name from the item name and remove commas
+    item_name = extract_item_name(match[6])
+
+    # Extract item level from the item name
+    item_level = extract_item_level(match[6])
+
+    # Replace 'None' with 'Unknown'
+    item_level = item_level or 'Unknown'
+
+    # Remove 'rr' and 'rr0' from the item level
+    item_level = item_level.replace('rr0', '').replace('rr', '')
+
+    # Extract item quality from the item name
+    quality = extract_item_quality(match[6])
+
+    # Extract item type from the item name
+    item_type = extract_item_type(match[6])
+
+    # Extract item trait from the item name
+    item_trait = extract_item_trait(match[6])
+
+    # Extract item type2 from the item name
+    item_type2 = extract_item_type2(match[6])
+
+    # Check if the transaction ID is already processed
+    if match[3] not in existing_transaction_ids:
+        # Add each match to the list of rows
+        rows_to_write.append([f'@{match[0]}', f'@{match[1]}', match[2], transaction_date_time, item_quantity_1, total_price, match[5], item_name, item_level, quality, item_type, item_trait, item_type2])
+        existing_transaction_ids.add(match[3])
+
+# Sort the rows by the transaction date in descending order (most recent first)
+rows_to_write.sort(key=lambda x: datetime.strptime(x[3], '%Y-%m-%d %H:%M:%S'), reverse=True)
+
+# Write the sorted rows to the CSV file
 with open('data.csv', 'a', newline='', encoding='utf-8') as csvfile:
     csv_writer = csv.writer(csvfile)
+    csv_writer.writerows(rows_to_write)
 
-    for match, quantity_match in zip(matches, quantity_matches):
-        # Extract item quantity from the quantity match
-        item_quantity_1, item_quantity_2 = quantity_match
-
-        # Append 'g' after the total price
-        total_price = f"{match[4]}g"
-
-        # Extract item name from the item name
-        item_name = extract_item_name(match[6])
-
-        # Extract item level from the item name
-        item_level = extract_item_level(match[6])
-
-        # Replace 'None' with 'Unknown'
-        item_level = item_level or 'Unknown'
-
-        # Remove 'rr' and 'rr0' from the item level
-        item_level = item_level.replace('rr0', '').replace('rr', '')
-
-        # Extract item quality from the item name
-        quality = extract_item_quality(match[6])
-
-        # Extract item type from the item name
-        item_type = extract_item_type(match[6])
-
-        # Extract item trait from the item name
-        item_trait = extract_item_trait(match[6])
-
-        # Extract item type2 from the item name
-        item_type2 = extract_item_type2(match[6])
-
-        # Check if the transaction ID is already processed
-        if match[3] not in existing_transaction_ids:
-            # Write each match to the CSV file including item quantity and transaction ID
-            csv_writer.writerow([f'@{match[0]}', f'@{match[1]}', match[2], match[3], item_quantity_1, total_price, match[5], item_name, item_level, quality, item_type, item_trait, item_type2])
-            existing_transaction_ids.add(match[3])
-
-print("Extraction and parsing completed. Check 'data.csv'")
+print("Extraction, parsing, and sorting completed. Check 'data.csv'")
